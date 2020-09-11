4adb32bd86c80f195fab8a12382d2d2b8f115a52
gil-arreola <gildardo-arreola@hotmail.com>
2020-09-10 19:53:00 -0500

    Avance 10-sep-22

--- app3.js
+++ app3.js
@@ -8,3 +8,30 @@ var addNumbers = function (a,b){
 var result = addNumbers (2, 8);
 
 console.log ( result );
+
+//
+// function nacimiento(){
+//     edad = Number (prompt ('Cual es tu edad?', '31'));
+//     anio = 2020 - edad;
+//     return anio;
+// }
+//
+//     console.log(nacimiento());
+//     document.write (' Por si no sabias, naciste el año ' + anio);
+
+function numeroMayor (valor1, valor2){
+    if (valor1>>valor2){
+    return valor1;
+    if (valor1=valor2)
+    return 'X';
+    else
+    return valor2;
+    }
+}
+
+
+valor1 = prompt ('ingresa un valor', "0");
+valor2 = prompt ('ingresa otro valor', "0");
+
+document.write(" Tu numero chido es el " + numeroMayor(valor1, valor2));
+console.log(" Tu numero chido es el " + numeroMayor(valor1, valor2));
